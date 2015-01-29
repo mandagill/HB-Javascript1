c754dc4db5cd651cfbcbe9861f73768e08ff7437
Hackbright Student <info@hackbrightacademy.com>
2015-01-28 17:54:50 -0800

    began exercise 2, still need to add a fix for color changing

--- final.js
+++ final.js
@@ -10,5 +10,14 @@ function changeFromHelloToWorld (evt) {
 getTextLink.addEventListener('click', changeFromHelloToWorld);
 
 // Second problem, change color of a div
+var getColorLink = document.getElementById("get-color");
 
+function changeDivColor (evt) {
+    evt.preventDefault();
+    divToChange = document.getElementById('text-box-color');
+    //FIXME this sets the attribute but doesn't change the color
+    divToChange.setAttribute('background-color', 'red');
+    divToChange.innerHTML = "I am the color red!";
+}
 
+getColorLink.addEventListener('click', changeDivColor);
